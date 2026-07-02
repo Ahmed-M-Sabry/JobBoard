ec802c9c70fa9cc031884d6d4d1a4b77d9bc0eb3
Ahmed Sabri <a7med.mohamed.sabri@gmail.com>
2026-07-02 12:12:16 +0300

    Add CVLink to JobApplication and related DTOs/migrations
    Added a nullable CVLink property to the JobApplication entity for storing applicant CV links. Updated EF Core migrations and model snapshot to include the new column. Introduced CreateJobApplication and EditJobApplication DTOs with CVLink support.

---
+++ JobBoard.Application/Dtos/JobApplication/EditJobApplication.cs
@@ -0,0 +1,15 @@
+﻿using JobBoard.Domain.Entities.Users;
+using JobBoard.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoard.Application.Dtos.JobApplication
+{
+    public class EditJobApplication
+    {
+        public string? CVLink { get; set; }
+    }
+}
---
+++ JobBoard.Infrastructure/Migrations/20260702090931_CVLinkToApplicationJobPostTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobBoard.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CVLinkToApplicationJobPostTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CVLink",
+                table: "JobApplications",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CVLink",
+                table: "JobApplications");
+        }
+    }
+}
